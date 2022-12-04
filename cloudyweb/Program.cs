0ea1ee86798497ff92a2868d412cca3d5eacf41a
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-12-04 10:05:07 +0100

    Add SSO

--- cloudyweb/Program.cs
+++ cloudyweb/Program.cs
@@ -1,16 +1,24 @@
 using cloudyweb.Models;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCloudy(cloudy => cloudy
-  .AddAdmin(admin => admin.Unprotect())   // NOTE: Admin UI will be publicly available!
+  .AddAdmin()
   .AddContext<Context>()                // Adds EF Core context with your content types
 );
 
 builder.Services.AddDbContext<Context>(options => options
   .UseCosmos(builder.Configuration["CosmosConnectionString"] ?? throw new Exception("CosmosEndpoint needed"), builder.Configuration["CosmosDatabase"] ?? throw new Exception("CosmosDatabase needed"))
 );
+
+builder.Services.Configure<AuthorizationOptions>(o => o.AddPolicy("adminarea", builder => builder.RequireAuthenticatedUser()));
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration);
 
 var app = builder.Build();
 
